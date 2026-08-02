9e9f8bb3199efcd47a4fa9dc36271ed5e1bd44d5
WhylerLab <development@noahwhyler.com>
2026-08-02 04:51:47 +0200

    update - engine.py some if/else

--- combat/engine.py
+++ combat/engine.py
@@ -1,8 +1,30 @@
 # combat/engine.py
+from exceptions.custom_exceptions import InvalidActionError
+
 
 def run_combat(player, enemy):
     while True:
-        # 1. Spieler wählt Aktion
+        print("""1. Angriff (Leicht)
+    2. Angriff (Schwer)
+    3. Abwehr
+    4. Heilung
+    5. Flucht""")
+
+        player_action = int(input("Was möchtest du tun: "))
+
+        if player_action == 1:
+            pass
+        elif player_action == 2:
+            pass
+        elif player_action == 3:
+            pass
+        elif player_action == 4:
+            pass
+        elif player_action == 5:
+            pass
+        else:
+            raise InvalidActionError()
+
 
         # 2. Falls Fliehen erfolgreich -> break (kein Gold, Kampf vorbei)
 
